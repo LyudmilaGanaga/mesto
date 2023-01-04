087313510782688f6d7bff9ac859211df54f5880
Mila <milanaganaga@yandex.ru>
2023-01-04 19:13:17 +0400

    Проектная работа 4

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+const elementPopup = document.querySelector(".popup");
+const elementOpenButton = document.querySelector(".profile__edit-button");
+const elementCloseButton = elementPopup.querySelector(".popup__close-button");
+const elementPopupForm = elementPopup.querySelector(".popup__form");
+const popupName = elementPopup.querySelector(".popup__input_data_name");
+const popupWorking = elementPopup.querySelector(".popup__input_data_working");
+const elementProfileName = document.querySelector(".profile__name");
+const elementProfileWorking = document.querySelector(".profile__working");
+
+// открыть popup
+elementOpenButton.addEventListener("click", () => {
+  elementPopup.classList.add("popup__opened");
+});
+
+// закрыть popup
+elementCloseButton.addEventListener("click", () => {
+  elementPopup.classList.remove("popup__opened");
+});
+
+// сохранить изменения
+elementPopupForm.addEventListener("submit", (submitFormHandler) => {
+  submitFormHandler.preventDefault();
+  elementProfileName.textContent = popupName.value;
+  elementProfileWorking.textContent = popupWorking.value;
+  elementPopup.classList.remove("popup__opened");
+});
